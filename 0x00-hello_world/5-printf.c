ad2923b318c0164ba6f4c1a574dcf2be2579e673
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-17 05:33:30 -0700

    PRINTF

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - is my main function
  * Return: Always zero for success
  */
-int main (void)
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
