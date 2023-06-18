7984c7d91ecde812acd37bc495073352a8aa2e76
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-18 14:14:04 -0700

    commited

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - my main function
+ *Description 'describe my code'
+ *Return: 0
+ */
+
+int main(void)
+{
+int n = 122;
+while (n >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
+}
