653cd3afbc7eaf5e228d4b99bb56f922b6925ac8
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-18 12:56:49 -0700

    while loop 2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**main - is my main function
+ *Description 'advanced while loop'
+ *Return: always zero for success
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+{
+	if (n == 101 || n == 113)
+	{
+	n++;
+	continue;
+	}
+	putchar (n);
+	n++;
+}
+return (0);
+}
