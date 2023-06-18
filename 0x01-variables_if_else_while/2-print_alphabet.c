d7adc9e5541abc328c4d189c7f6b79514f22c7d3
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-18 12:16:15 -0700

    whileloop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Is my main finction
+ *Return: always equal zwerp for success
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar (n);
+		n++;
+	}
+	putchar ('\n');
+return (0);
+}
