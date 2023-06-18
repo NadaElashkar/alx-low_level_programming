889f8c7b7793978ee0d0ad2670819526210972ac
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-18 14:58:20 -0700

    commited

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main -- my main function
+ *Description 'describes my code'
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57 ; n++)
+		{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+		}
+putchar('\n');
+return (0);
+}
