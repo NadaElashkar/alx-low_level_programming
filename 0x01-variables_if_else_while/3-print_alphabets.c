9c61a81dd94823ba0c73932dda1f73717545cb0b
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-18 12:35:29 -0700

    second while loop

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - My main function
+ *Return: return always zero for success
+ */
+int main(void)
+{
+	int n = 97;
+	int i = 65;
+
+	while (n <= 122)
+	{
+		putchar (n);
+		n++;
+	}
+	while (i <= 90)
+	{
+		putchar (i);
+		i++;
+	}
+putchar ('\n');
+return (0);
+}
+
