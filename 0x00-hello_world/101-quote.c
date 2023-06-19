223b4daa04034bffde6ab31c1ceb93e58dd7a221
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-19 01:08:28 -0700

    commited

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - my main function
+ *Description 'My code description'
+ *Return: 1
+ */
+
+int main(void)
+{
+write (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+
+
+return (1);
+}
