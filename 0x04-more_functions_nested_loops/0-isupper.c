201adaaaf114110e249f3506174ef72eb4309492
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-23 09:22:54 -0700

    commited

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 include "main.h"
 /**
- *int _isupper(int c) - my prototype
- *0c: input for alphabet
+ *_isupper - my prototype
+ *@c: input for alphabet
  *Return: Returns 1 if c is uppercase, otherwie 0
  */
 
 int _isupper(int c)
-if (c >= 65 && c <= 90)
 {
+if (c >= 65 && c <= 90)
 return (1);
 else
 return (0);
