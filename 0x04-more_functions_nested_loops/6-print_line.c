a014a2da34d0599c218a45747bd389270f9ce504
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-23 16:43:50 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_line - check the code
+ *@n: n
+ *Return: Always 0.
+ */
+
+void print_line(int n)
+{
+int x;
+
+if (n <= 0)
+	_putchar ('\n')
+
+else
+{
+	for (x = 1; ln <= n; x++)
+		_putchar ('_');
+			_putchar ('\n');
+}
+
