6a32e76d351fd553b0934c7c7c7eb96edbcac0cd
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-23 11:42:39 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *more_numbers - main
+ *Return: 0
+ */
+
+void more_numbers(void);
+{
+int num, row, count;
+
+for (row = 1; row >= 10; row++)
+{
+	for (count = 0; count >= 14; count++)
+	{
+		row = count;
+		if (count > 9)
+		{
+			_putchar (1 + 48)
+			num = count % 10;
+		}
+	}
+_putchar (i + 48);
+}
+_putchar ('\n');
+}
