78137bed5cf2d6d422fd316c5fec229e47f80bab
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-23 17:11:53 -0700

    commited

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_diagonal - check the code
+ *@n: n
+ *Return: Always 0.
+ */
+
+void print_diagonal(int n)
+int x.postn;
+
+if (n <= 0)
+_putchar ('\n');
+else
+{
+for (x = 1; x <= postn; x++)
+_putchar ('92');
+}
+
