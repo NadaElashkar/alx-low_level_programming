62d3a2e98746a4d323f79fd4eb02a1757a4256d8
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-25 18:41:12 -0700

    commited

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - check the code
+ *@i: i
+ *Return: Always 0.
+ */
+
+int _abs(int);
+
+if (i >= 0)
+{
+return (i);
+}
+if (i <0)
+{
+return (i * -1);
+}
