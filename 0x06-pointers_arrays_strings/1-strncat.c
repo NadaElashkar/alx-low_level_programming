8e58a5c238b48168a205e8b36368632beadae740
NadaElashkar <nada.elashkar92@gmail.com>
2023-06-29 13:53:56 -0700

    commited

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,14 +15,14 @@ char *_strncat(char *dest, char *src, int n)
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		i++
+		i++;
 	}
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-	desr[i] = src[j];
-	i++
-	j++
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
 	dest[i] = '\0';
 	return (dest);
